cfbf97896107bae05710c99752ee8c384ee7a2a3
muhazAhmed <muhazvla313@gmail.com>
2024-09-06 21:24:29 +0530

    added media types

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            outputPath: "static/videos",
+            publicPath: "/_next/static/videos",
+            name: "[name].[ext]",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
